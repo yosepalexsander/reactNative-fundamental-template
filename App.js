b6e568c783b160eb8f1cd9aabc8dc73197b43e73
yosep <yosepalexsander9@gmail.com>
2021-11-17 09:39:23 +0700

    done component

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
+import EmbedExpression from "./src/screens/embedExpression";
 
 //Import Screen
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 50 }}>
       <StatusBar />
-      {/* Use Component */}
+      <EmbedExpression />
     </View>
   );
 }
--- src/screens/embedExpression.js
+++ src/screens/embedExpression.js
@@ -1,16 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function EmbedExpression() {
   // Create Function Here
+  function getMajor(batch) {
+    return "Full Stack " + batch;
+  }
 
   // Create a variable Here
+  const companyName = "dumbways.id";
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text>
+        Welcome to {companyName} {getMajor(27)}
+      </Text>
     </View>
   );
 }
